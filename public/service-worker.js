b481ee7ae50ca5dbfc7be37d0272d3cd91440660
amit karmacharya <amitkarmacharya@MacBook-Pro.local>
2021-01-06 10:57:22 -0500

    created service worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "budget-tracker-v-1";
+const RUNTIME = "runtime";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/style.cs",
+    "/assets/js/index.js"
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+});
+  
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+    const currentCaches = [CACHE_NAME, RUNTIME];
+    event.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        })
+        .then((cachesToDelete) => {
+          return Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+});
+  
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+  
+          return caches.open(RUNTIME).then((cache) => {
+            return fetch(event.request).then((response) => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    }
+});
