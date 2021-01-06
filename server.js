db31549c382a47e7b5f11971c4a8307778fa7288
amit karmacharya <amitkarmacharya@MacBook-Pro.local>
2021-01-06 12:55:47 -0500

    added process.env.PORT and routes to / for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -21,6 +21,7 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes
+app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
